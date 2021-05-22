b907998170c27200708d46e3d186ffffae9ccb80
Analyn <analyncajocson@gmail.com>
2021-05-22 09:43:41 +0800

    Creates initial form

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>React with Typescript</h1>
+
+      <form>
+        <label htmlFor="userName">Name:</label>
+        <input type="text"
+          id="userName"
+          name="name"
+          value=""
+        />
+
+        <label htmlFor="userAge">Age:</label>
+        <input type="number"
+          id="userName"
+          name="age"
+          value=""
+        />
+
+        <label htmlFor="userJob">Job:</label>
+        <input type="text"
+          id="userJob"
+          name="job"
+          value=""
+        />
+
+        <button type="submit">Add user</button>
+
+      </form>
     </div>
+    
   );
 }
 
